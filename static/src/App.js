9db62611dd26dd14eee5457257b603e88e3d5fae
teck7 <teckyap@hotmail.com.au>
2017-06-29 21:15:06 +1000

    implement react route

--- static/src/App.js
+++ static/src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+          <main>
+              <nav>
+                  <Link to='/'>Home</Link>
+                  <Link to='/about'>About</Link>
+                  <Link to='/faq'>Faq</Link>
+                  <Link to='/contact'>Contact</Link>
+              </nav>
+
+              <Route exact path='/' render={
+                  () => (
+                    <div>Home Page</div>
+                  )
+                }
+              />
+
+              <Route path='/about' render={
+                  () => (
+                    <div>About Page</div>
+                  )
+                }
+              />
+
+              <Route path='/faq' render={
+                  () => (
+                    <div>FAQ Page</div>
+                  )
+                }
+              />
+
+              <Route path='/contact' render={
+                  () => (
+                    <div>Contact Page</div>
+                  )
+                }
+              />
+
+          </main>
+      </Router>
+
+
+
+
     );
   }
 }
